d9d6ddc2febd54563b44bc14664274018b87336d
Rajeswari-lang <104066289+Rajeswari-lang@users.noreply.github.com>
2022-04-20 15:49:04 +0530

    Add files via upload

---
+++ Whole Number.js
@@ -0,0 +1,13 @@
+function Whole Number(num1){
+var num1=prompt("enter first number");
+
+if(num1>=0){
+document.write("<h2>");
+document.writeln(num1+" is Whole Number");
+document.write("<h2>");
+}else{
+document.write("<h2>");
+document.writeln(num2+" is not Whole Number");
+document.write("<h2>");
+}
+}
